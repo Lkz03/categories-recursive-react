283dfef4aa5d3721271a04b71ecab15b169d5735
Lkz03 <andriuspr21@gmail.com>
2021-10-10 14:17:56 +0300

    master

--- src/features/Handler.js
+++ src/features/Handler.js
@@ -5,13 +5,14 @@ import Value from "../components/Value";
 const Handler = ({passValue, parent}) => {
     const [thisValue, setThisValue] = useState(0)
     const [total, setTotal] = useState(0)
-    const [mem, setMem] = useState(0)
 
     const submitHandler = (e, bool) => {
-        console.log(bool)
-        {bool === true ? setThisValue(e) : ''}
-        {parent ? '' : passValue(e - mem + total)}
-        setMem(e+total)
+        {(bool === true) ? (
+            setThisValue(e)
+        ) : (
+            setTotal(e)
+        )}
+        { parent ? '' : passValue(e - thisValue - total)}
     }
 
     return (
@@ -19,7 +20,7 @@ const Handler = ({passValue, parent}) => {
             {total === 0 ? ('') : (
             <React.Fragment> Total: {total} </React.Fragment>
                  )}
-            {thisValue === 0 ? <React.Fragment><Value returnValue={(e, bool) => submitHandler(e, bool)}/> <Category passValue={e => {submitHandler(e - mem), setTotal(e)}}/> </React.Fragment> :
+            {thisValue === 0 ? <React.Fragment><Value returnValue={(e, bool) => submitHandler(e, bool)}/> <Category passValue={e =>submitHandler(e, false)}/> </React.Fragment> :
             <React.Fragment><Value returnValue={(e, bool) => submitHandler(e, bool)} /> Value: {thisValue}</React.Fragment>}
          </div>
     )
